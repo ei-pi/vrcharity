0096308b2cc4b384f869da0b37db631c1269486e
ei-pi <91853103+ei-pi@users.noreply.github.com>
2022-10-04 20:15:27 -0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# VR Charity website build changelog
+
+## v0.1.0.1 (10/04/2022)
+- First build
--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 {
-    const header = document.querySelector("header");
+    const header = document.querySelector("header"),
+        mobileLayout = window.matchMedia("screen and (max-width: 800px)");
 
     header.addEventListener("click", ev => {
-        if (!ev.button && window.matchMedia("screen and (max-width: 750px)").matches) {
+        if (!ev.button && mobileLayout.matches) {
             header.classList.toggle("collapsed");
             header.classList.toggle("expanded");
         }
