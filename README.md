202171ca8cf4a79187ef4a076d5225a8b44cb3c4
ei-pi <91853103+ei-pi@users.noreply.github.com>
2022-10-05 23:11:03 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # VR Charity website build changelog
 
+## v0.1.0.2 (10/05/2022)
+- Animated the main logo (fade in + translate)
+- Adjusted some header links' positions
+- Fixed horizontal scrolling for mobile
+- Made the dropdown more robust
+- Started adding the games section
+
 ## v0.1.0.1 (10/04/2022)
 - First build
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 {
     const header = document.querySelector("header"),
-        mobileLayout = window.matchMedia("screen and (max-width: 800px)");
+        linkCont = document.querySelector("div#link-container"),
+        mobileLayout = window.matchMedia("screen and (max-width: 1000px)");
 
     header.addEventListener("click", ev => {
         if (!ev.button && mobileLayout.matches) {
             header.classList.toggle("collapsed");
-            header.classList.toggle("expanded");
+            header.style.height = linkCont.style.height = header.classList.toggle("expanded") ? `${linkCont.scrollHeight}px` : "";
         }
     });
 }
